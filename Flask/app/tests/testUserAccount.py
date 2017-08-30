0a4b8120d7b24a59c35dfa24efcf582303566add
kosgei <kosgei@mail.com>
2017-08-30 07:45:04 +0300

    User Registration test written.Test failed

--- Flask/app/tests/testUserAccount.py
+++ Flask/app/tests/testUserAccount.py
@@ -1,76 +1,47 @@
 """ User Registration tests"""
 import unittest
 
+def createUser(email, username, password, cPassword):
+        user_list = []
+        users = {}
 
-
+        for user in user_list:
+            if email == user['email']:
+                return " User exists. Please login"
+            else:
+                if password == cPassword:
+                    users['username'] = username
+                    users['email'] = email
+                    users['password'] = password
+                    user_list.append(users)
+                else:
+                    return "Password does not match"
+        return "Login, Registration successful"
 
 class UserTestCases(unittest.TestCase):
 	"""
 	Test contains tests for:
 	password and cofirm password match
+	User already exists
 	Correct input given during registration and login 
 	"""
 
 	def test_mismatched_password(self):
-		msg = self.user.createUser(
-			"junenthemba@gmail.com","njune","Qwerty1","Qwerty")
-		self.assertEqual(msg,"PasswordMismatch")
+		res = "junenthemba@gmail.com","njune","Qwerty1","Qwerty"
+		self.assertEqual(createUser("junenthemba@gmail.com","njune","Qwerty1","Qwerty"),"Password does not match")
+
 
 	def test_match_password(self):
-		msg = self.user.createUser(
-			"junenthemba@gmail.com","njune","Qwerty1","Qwerty1")
-		self.assertEqual(msg,"Welcome")
+		res="junenthemba@gmail.com","njune","Qwerty1","Qwerty1"
+		self.assertEqual(createUser("junenthemba@gmail.com","njune","Qwerty1","Qwerty1"),"Login, Registration successful")
 
 	def test_user_exists(self):
-		self.user.createUser(
-			"junenthemba@gmail.com","njune","Qwerty1","Qwerty1")
-		msg= self.user.createUser(
-			"junenthemba@gmail.com","njune","Qwerty1","Qwerty1")
-		self.asserEqual(msg, "User already exists")
+		res="junenthemba@gmail.com","njune","Qwerty1","Qwerty1"
+		self.assertEqual(createUser("junenthemba@gmail.com","njune","Qwerty1","Qwerty1"), "User exists. Please login")
 
 	def test_Successful_registration(self):
-		msg =self.user.createUser(
-			"liamTunoe@gmail.com","ltunoe","asdrt3","asdrt3")
-		self.asserEqual(msg,"Login, Registration successful")
-
-	def test_correct_input(self):
-		msg = self.user.createUser(
-			"liamTunoe@gmail.com","ltunoe","asdrt3","asdrt3")
-		self.asserEqual(msg,"Login, Registration successful")
-
-
-
-	""" 
-	Test Cases for User Login 
-	"""
-
-
-	def test_account_doesnot_exists(self):
-		self .user.users=[
-			{'username': 'njune', 'email':'junenthemba@gmail.com', 'password':'Qwerty1'}]
-		msg= self.user.signin("Cmilka","zypks2")
-		self.asserEqual(msg,"Please Create account to login")
-
-	def test_user_exists(self):
-		self .user.users=[
-			{'username': 'njune', 'email':'junenthemba@gmail.com', 'password':'Qwerty1'}]
-		msg= self.user.signin("njune","Qwerty1")
-		self.asserEqual(msg,"Welcome, Create a new shopping list")
-	
-
-	def test_wrong_username(self):
-		self.user.users =[{
-		'username': 'njune', 'email':'junenthemba@gmail.com', 'password':'Qwerty1'
-		}]
-		msg=self.user.signin("njunr","Qwerty1")
-		self.asserEqual(msg,"Wrong Username given.Please try again")
-
-	def test_wrong_password(self):
-		self.user.users =[{
-		'username': 'njune', 'email':'junenthemba@gmail.com', 'password':'Qwerty1'
-		}]
-		msg=self.user.signin("njune","Qwerty2")
-		self.asserEqual(msg,"Wrong passsword given.Please try again")
+		res="liamTunoe@gmail.com","ltunoe","asdrt3","asdrt3"
+		self.assertEqual(createUser("liamTunoe@gmail.com","ltunoe","asdrt3","asdrt3"),"Login, Registration successful")
 
 
 if __name__ == '__main__':
